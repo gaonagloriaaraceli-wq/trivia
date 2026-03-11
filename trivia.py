8bea0181c99bcee930fa4ffb161cb36c570c10cf
Gloria Gaona <gaonagloriaaraceli@gmail.com>
2026-03-11 17:16:08 -0300

    agregue las preguntas

--- trivia.py
+++ trivia.py
@@ -1,2 +1,7 @@
 nombre_usuario=input("Ingrese su nombre")
 print(f"Bienvenid@ {nombre_usuario} a esta trivia ")
+
+pregunta1=input("Cuánto es 2+2?")
+pregunta2=input("Cuánto es 6-2?")
+pregunta3=input("Cuanto es 2 x 3?")
+pregunta4=input("Cuánto es 4/2?")
